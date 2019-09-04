2390a90c89f78229bd2697e08759b1533546da92
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-09-04 15:37:08 -0700

    minor change

--- back-end/server.js
+++ back-end/server.js
@@ -44,11 +44,11 @@ app.get('/', (req, res) => {
 
 
 
-// app.use('/api/v1/auth', routes.auth);
+app.use('/api/v1/auth', routes.auth);
 
-// app.use('/api/v1/users', routes.users);
+app.use('/api/v1/users', routes.users);
 
-// app.use('/api/v1/posts', routes.posts);
+app.use('/api/v1/posts', routes.posts);
 
 
 

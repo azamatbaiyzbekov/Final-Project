c949b7b3ccd6cd2c6e8ae26e81aeff5dcca0eb0e
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-09-04 11:53:40 -0700

    adds user and post models

---
+++ back-end/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    join_date: {
+        type: Date,
+        default: Date.now,
+    },
+    place_of_birth: {
+        type: String,
+    },
+    occupation: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: Boolean,
+    default: false,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
